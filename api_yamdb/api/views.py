41688f0f0f8f8000e0c860e3213e456a5b1d572d
Андрей Пашовкин <andre.pashovkin@yandex.ru>
2023-03-17 09:29:43 +0200

    add manage command for load data from csv files

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -8,7 +8,6 @@ from rest_framework import (
     status,
     filters,
     permissions,
-    mixins
 )
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
